6891a2d550a66e374c5da441b452256abccaffad
Thomas Ashelford <thomas@ether.com.au>
2014-11-12 14:00:26 +1100

    ...properly

---
+++ py/__init__.py
@@ -0,0 +1,28 @@
+{
+  "name": "colorly",
+  "version": "0.0.3",
+  "description": "Comprehensive library of color books from several color systems - implemented in Sass, LESS, Stylus, JSON, and CSV.",
+  "scripts": {
+    "test": "mocha"
+  },
+  "author": "James Pederson",
+  "license": "MIT",
+  "repository": {
+    "type": "git",
+    "url": "git://github.com/jpederson/Colorly.git"
+  },
+  "dependencies": {
+    "fastclick": "~1.0.2",
+    "jquery": "~1.11.1"
+  },
+  "devDependencies": {
+    "grunt": "latest",
+    "grunt-contrib-sass": "*",
+    "grunt-contrib-stylus": "*",
+    "grunt-contrib-less": "*",
+    "grunt-contrib-watch": "*",
+    "grunt-contrib-uglify": "*",
+    "grunt-shell": "*",
+    "matchdep": "*"
+  }
+}
